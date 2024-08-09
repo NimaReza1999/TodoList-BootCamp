1fee1b0247afa066a587f06a6149419a6b6b8d77
Nima Reza <NimaaRezaa99@gmail.com>
2024-08-09 19:34:17 +0330

    basic changes done

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,11 @@ const openModal = document.querySelector('.nav__action');
 const modal = document.querySelector('.modal__todo');
 const closeModal = document.querySelector('.todo__close-btn');
 const confirmTodo = document.querySelector('.todo__confirm-btn');
+const todoTitleInput = document.querySelector('.todo__title');
+const radioButtons = document.querySelectorAll('.radio__button');
+
+const radioButtonsArray = [...radioButtons];
+
 
 
 // *Functions
@@ -21,10 +26,26 @@ function confirmTodoFunction() {
 function closeModalFunction() {
     
     modal.style.display = 'none';
+    todoTitleInput.value = "";
+    radioButtons.forEach(element => element.checked =! true)
+
 }
+
+function selectOneRadioButton(element) {
+    
+    // console.log (element.target.id)
+}
+
+function todoCreator() {
+
+}
+
 
 // *EventListners
 
 openModal.addEventListener('click', openModalFunction);
 closeModal.addEventListener('click', closeModalFunction);
 confirmTodo.addEventListener('click', confirmTodoFunction);
+radioButtons.forEach((element) => {
+    element.addEventListener('click', selectOneRadioButton);
+})
